98b910c3cb215968850fe5c45e33d9200b16af0e
Julian Herrera <juliansantiago.rubianoherrera@gmail.com>
2025-08-13 11:14:58 -0500

    Server creation (html and css called) and added json data (example),

--- server.js
+++ server.js
@@ -1,7 +1,35 @@
-import http from 'http'
+import http from 'http';
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const server = http.createServer((req,res)=>{
-    res.end('Hello world')
+
+const server = http.createServer(async(req,res)=>{
+    try{
+        const {method,url} = req
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = path.dirname(__filename);
+    
+        if(url === '/' || url === '/index.html'){
+            
+            const route = path.join(__dirname,'public','index.html')
+            const html = await fs.readFile(route,'utf-8')
+            res.writeHead(200,{'content-type':'text/html'})
+            res.end(html)
+        }else if (url.endsWith('.css')){
+            const route = path.join(__dirname,'public',url)
+            const css = await fs.readFile(route,'utf-8')
+            res.writeHead(200,{'content-type':'text/css'})
+            res.end(css)
+        } else{
+            res.writeHead(404,{'content-type':'text/plain'})
+            res.end('Error 404, pagina no encontrada')
+        }
+    }catch(error){
+        res.writeHead(500,{'content-type':'text/plain'})
+        res.end('Error al conectar con el servidor')
+        console.error(error)
+    }
 })
 
 const PORT = 3000
@@ -9,3 +37,4 @@ const PORT = 3000
 server.listen(3000,()=>{
     console.log(`The server is open in the port ${PORT}`)
 })
+
